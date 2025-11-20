bc653100e76ae12eb727792ed541234e3a58be70
Muhammed Salim <salimmhdkeloth@gmail.com>
2025-11-20 15:27:25 +0530

    Update app.py

--- Khushiyaan-Foundation-Dashboard-documentation/app.py
+++ Khushiyaan-Foundation-Dashboard-documentation/app.py
@@ -44,5 +44,5 @@ app.clientside_callback(
     dash.Input("interval", "n_intervals")
 )
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=False)
